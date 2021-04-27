a70806bbff5c625c97168979cd34e462d1ecd6fe
NeverDecaf <jtr9289@gmail.com>
2021-04-27 02:47:09 -0400

    init

---
+++ fetch.js
@@ -0,0 +1,62 @@
+const EVENT_URL = 'https://hk4e-api-os.mihoyo.com/common/hk4e_global/announcement/api/getAnnContent?game=hk4e&game_biz=hk4e_global&lang=en&bundle_id=hk4e_global&platform=pc&region=os_usa&level=60'
+const CORS_PROXY = "https://jsonp.afeld.me/?url="
+// fetch("test.json")
+fetch(CORS_PROXY+encodeURIComponent(EVENT_URL)+'&t='+Math.floor(new Date().getTime()/1000))
+.then(res => res.json())
+.then(json => {
+	if (json.retcode == 0 && json.message == 'OK')
+	{
+		Array.from(json.data.list).sort((a,b) => b.ann_id-a.ann_id).forEach(article => {
+			let wrapper = document.createElement('div');
+			let title = document.createElement('div');
+			let subtitle = document.createElement('div');
+			let banner = document.createElement('img');
+			let content = document.createElement('div');
+			wrapper.classList.add('_article');
+			
+			banner.src = article.banner;
+			content.appendChild(banner);
+			content.innerHTML += article.content.replace(/javascript:miHoYoGameJSSDK\.openInBrowser\('([^)]+)'\);/gi,'$1" target="_blank').replace(/&lt;(\/?t(?:| )[^&]*)&gt;/gi,'<$1>');
+			
+			title.innerHTML = article.title;
+			title.classList.add('title');
+			title.classList.add('article__title');
+			
+			
+			wrapper.setAttribute('id',article.ann_id);
+			// wrapper.appendChild(content);
+			// recreate news site structure so we can just reuse css
+			content.classList.add('article__content');
+			let body = document.createElement('div');
+			body.classList.add('body');
+			let cate = document.createElement('div');
+			cate.classList.add('article');
+			cate.classList.add('cate');
+			cate.appendChild(title);
+			cate.appendChild(content);
+			body.appendChild(cate);
+			wrapper.appendChild(body);
+			
+			
+			document.getElementById('right').appendChild(wrapper);
+			
+			subtitle.innerHTML = article.subtitle;
+			subtitle.classList.add('subtitle');
+			subtitle.setAttribute('ann_id',article.ann_id);
+			subtitle.onclick = ()=> {
+				document.getElementsByClassName('_article');
+				Array.from(document.getElementsByClassName('_article')).forEach(e=> {e.classList.remove('_selected')});
+				document.getElementById(article.ann_id).classList.add('_selected');
+				Array.from(document.getElementsByClassName('subtitle')).forEach(e=> {e.classList.remove('_selected')});
+				subtitle.classList.add('_selected');
+				window.location.hash = article.ann_id;
+			}
+			document.getElementById('left').appendChild(subtitle);
+		});
+		
+		let first = document.getElementsByClassName('subtitle')[0];
+		first.classList.add('_selected');
+		document.getElementById(first.getAttribute('ann_id')).classList.add('_selected');
+		Array.from(document.getElementsByClassName('subtitle')).forEach(e=> {if (e.getAttribute('ann_id')==window.location.hash.substring(1)) {e.click()}});
+	}
+})
